5b9c94f5bfb6cc8d1bdae10dfdb9169f1de07410
leandrocgsi <leandrocgsi@gmail.com>
2023-02-16 15:30:18 -0300

    First Steps in JUnit 5 with Java 19

--- S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -6,13 +6,93 @@ import org.junit.jupiter.api.Test;
 
 class SimpleMathTest {
 
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @Test
-    void testSum() {
+    void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        
+        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
         
-        assertEquals(expected, actual);
+        assertEquals(expected, actual,
+            () -> firstNumber + "+" + secondNumber +
+            " did not produce " + expected + "!");
+    }
+    
+    @Test
+    void testSubtraction() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        
+        Double actual = math.subtraction(firstNumber, secondNumber);
+        double expected = 4.2D;
+        
+        assertEquals(expected, actual,
+                () -> firstNumber + "-" + secondNumber +
+                " did not produce " + expected + "!");
+    }
+    
+    @Test
+    void testMultiplication() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        
+        Double actual = math.multiplication(firstNumber, secondNumber);
+        double expected = 12.4D;
+        
+        assertEquals(expected, actual,
+                () -> firstNumber + "*" + secondNumber +
+                " did not produce " + expected + "!");
+    }
+    
+    @Test
+    void testDivision() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        
+        Double actual = math.division(firstNumber, secondNumber);
+        double expected = 3.1D;
+        
+        assertEquals(expected, actual,
+                () -> firstNumber + "/" + secondNumber +
+                " did not produce " + expected + "!");
     }
 
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @Test
+    void testDivision_When_FirstNumberIsDividedByZero_ShouldThrowArithmeticException() {
+        fail();
+    }
+    
+    @Test
+    void testMean() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        
+        Double actual = math.mean(firstNumber, secondNumber);
+        double expected = 4.1D;
+        
+        assertEquals(expected, actual,
+            () -> "(" + firstNumber + "+" + secondNumber + ")/2" +
+            " did not produce " + expected + "!");
+    }
+
+    @Test
+    void testSquareRoot() {
+        SimpleMath math = new SimpleMath();
+        double number = 81D;
+        double expected = 9D;
+        
+        Double actual = math.squareRoot(number);
+        
+        assertEquals(expected, actual,
+            () -> "Square Root of " + number +
+            " did not produce " + expected + "!");
+    }
 }
