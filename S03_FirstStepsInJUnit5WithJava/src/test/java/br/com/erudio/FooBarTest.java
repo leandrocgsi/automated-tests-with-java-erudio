858e99fcb8bef9613965bc49aac0cc8e03658870
leandrocgsi <leandrocgsi@gmail.com>
2023-02-16 14:38:37 -0300

    First Steps in JUnit 5 with Java 19

---
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/erudio/FooBarTest.java
@@ -0,0 +1,15 @@
+package br.com.erudio;
+
+// import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+    @Test
+    void test() {
+        // fail("Not yet implemented");
+        System.out.println("Hello JUnit 5!");
+    }
+
+}
