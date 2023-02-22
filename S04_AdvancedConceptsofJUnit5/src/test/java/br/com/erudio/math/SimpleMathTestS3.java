5ccad70d98848c39afbdffa6352a68f2a486c210
leandrocgsi <leandrocgsi@gmail.com>
2023-02-22 10:52:15 -0300

    Advanced Concepts of JUnit 5

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS3.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS3.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
-class SimpleMathTest {
+class SimpleMathTestS3 {
 
     SimpleMath math;
     
