8a26e3550090daf52de4fe2b428e1011faa57383
leandrocgsi <leandrocgsi@gmail.com>
2023-02-22 09:33:09 -0300

    First Steps in JUnit 5 with Java 19

--- S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -2,6 +2,10 @@ package br.com.erudio.math;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,15 +13,39 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Test Math Operations in SimpleMath Class")
 class SimpleMathTest {
 
+    SimpleMath math;
+    
+    @BeforeAll
+    static void setup() {
+        System.out.println("Running @BeforeAll method!");
+    }
+    
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Running @AfterAll method!");
+    }
+    
+    @BeforeEach
+    void beforeEachMethod() {
+        math = new SimpleMath();
+        System.out.println("Running @BeforeEach method!");
+    }
+    
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("Running @AfterEach method!");
+    }
+    
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @Test
     @DisplayName("Test 6.2 + 2 = 8.2")
     void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
 
+        System.out.println("Test 6.2 + 2 = 8.2");
+        
         // AAA Arrange, Act Assert
         // Given / Arrange
-        SimpleMath math = new SimpleMath();
-        
+                
         double firstNumber = 6.2D;
         double secondNumber = 2D;
         double expected = 8.2D;
@@ -34,7 +62,9 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 - 2 = 4.2")
     void testSubtraction() {
-        SimpleMath math = new SimpleMath();
+        
+        System.out.println("Test 6.2 - 2 = 4.2");
+        
         double firstNumber = 6.2D;
         double secondNumber = 2D;
         
@@ -49,7 +79,9 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 * 2 = 12.4")
     void testMultiplication() {
-        SimpleMath math = new SimpleMath();
+
+        System.out.println("Test 6.2 * 2 = 12.4");
+        
         double firstNumber = 6.2D;
         double secondNumber = 2D;
         
@@ -64,7 +96,9 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test 6.2 / 2 = 3.1")
     void testDivision() {
-        SimpleMath math = new SimpleMath();
+
+        System.out.println("Test 6.2 / 2 = 3.1");
+        
         double firstNumber = 6.2D;
         double secondNumber = 2D;
         
@@ -81,13 +115,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test Division by Zero")
     void testDivision_When_FirstNumberIsDividedByZero_ShouldThrowArithmeticException() {
+
+        System.out.println("Test Division by Zero");
         fail();
     }
     
     @Test
     @DisplayName("Test (6.2 + 2) / 2 = 4.1")
     void testMean() {
-        SimpleMath math = new SimpleMath();
+
+        System.out.println("Test (6.2 + 2) / 2 = 4.1");
+        
         double firstNumber = 6.2D;
         double secondNumber = 2D;
         
@@ -102,7 +140,9 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test Square Root of 81 = 9")
     void testSquareRoot() {
-        SimpleMath math = new SimpleMath();
+
+        System.out.println("Test Square Root of 81 = 9");
+        
         double number = 81D;
         double expected = 9D;
         
@@ -117,6 +157,8 @@ class SimpleMathTest {
     @DisplayName("Display Name")
     @Test
     void testABCD_When_XYZ_Should() {
+
+        System.out.println("Test BDD");
         // Given / Arrange
         // When / Act
         // Then / Assert
