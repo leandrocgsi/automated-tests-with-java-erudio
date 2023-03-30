42f0670b40e00202d15814b8bf68f3358d5e0da6
leandrocgsi <leandrocgsi@gmail.com>
2023-03-30 17:09:01 -0300

    Mockito's First Steps

---
+++ S06_MockitosFirstSteps/src/main/java/br/com/erudio/business/CourseBusiness.java
@@ -0,0 +1,33 @@
+package br.com.erudio.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.erudio.service.CourseService;
+
+// CourseBusiness = SUT - System (Method) Under Test
+public class CourseBusiness {
+
+    // CourseService is a Dependency
+    private CourseService service;
+
+    public CourseBusiness(CourseService service) {
+        this.service = service;
+    }
+    
+    public List<String> retriveCoursesRelatedToSpring(String student) {
+        
+        var filteredCourses = new ArrayList<String>();
+        if("Foo Bar".equals(student)) return filteredCourses;
+        var allCourses = service.retrieveCourses(student);
+        
+        for (String course : allCourses) {
+            if (course.contains("Spring")) {
+                filteredCourses.add(course);
+            }
+        }
+        
+        return filteredCourses;
+    }
+    
+}
---
+++ S06_MockitosFirstSteps/src/main/java/br/com/erudio/service/CourseService.java
@@ -0,0 +1,10 @@
+package br.com.erudio.service;
+
+import java.util.List;
+
+public interface CourseService {
+
+    public List<String> retrieveCourses(String student);
+    public List<String> doSomething(String student);
+    
+}
