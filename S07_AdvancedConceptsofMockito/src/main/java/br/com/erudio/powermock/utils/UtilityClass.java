b55a9c16d0182a008bbadd1913190635d7f10ddf
leandrocgsi <leandrocgsi@gmail.com>
2023-04-04 14:34:14 -0300

    PowerMock, JUnit 5 and Mockito

--- S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/powermock/utils/UtilityClass.java
+++ S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/powermock/utils/UtilityClass.java
@@ -5,6 +5,6 @@ public class UtilityClass {
     public static int staticMethod(long value) {
         // Some complex logic is done here...
         throw new RuntimeException(
-                "I dont want to be executed. I will anyway be mocked out.");
+            "I dont want to be executed. I will anyway be mocked out.");
     }
 }
