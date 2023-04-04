6d59731f9d28d81e0049ad7439a6177cc577e4d3
leandrocgsi <leandrocgsi@gmail.com>
2023-04-04 14:29:35 -0300

    PowerMock, JUnit 5 and Mockito

---
+++ S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/powermock/Dependency.java
@@ -0,0 +1,7 @@
+package br.com.erudio.powermock;
+
+import java.util.List;
+
+interface Dependency {
+	List<Integer> retrieveAllStats();
+}
---
+++ S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/powermock/utils/UtilityClass.java
@@ -0,0 +1,10 @@
+package br.com.erudio.powermock.utils;
+
+public class UtilityClass {
+    
+	public static int staticMethod(long value) {
+		// Some complex logic is done here...
+		throw new RuntimeException(
+				"I dont want to be executed. I will anyway be mocked out.");
+	}
+}
