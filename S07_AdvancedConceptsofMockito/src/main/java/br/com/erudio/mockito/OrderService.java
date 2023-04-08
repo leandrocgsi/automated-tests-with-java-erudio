4ac743c3cfd42d8bae8544f6387464fc5dc493a9
leandrocgsi <leandrocgsi@gmail.com>
2023-04-08 14:51:38 -0300

    Advanced Concepts of Mockito - Mocking Constructors and Static Methods with JUnit 5 and Mockito

--- S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/mockito/OrderService.java
+++ S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/mockito/OrderService.java
@@ -6,7 +6,9 @@ import java.util.UUID;
 public class OrderService {
 
     public Order createOrder(String productName, Long amount, String OrderID) {
+        
         Order order = new Order();
+        
         order.setId(OrderID == null ? UUID.randomUUID().toString() : OrderID);
         order.setCreationDate(LocalDateTime.now());
         order.setAmount(amount);
