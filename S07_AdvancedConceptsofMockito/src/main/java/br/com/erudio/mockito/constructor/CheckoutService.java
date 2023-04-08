3998481b52cdf2bbff34245c5f044d8498aa2d7b
leandrocgsi <leandrocgsi@gmail.com>
2023-04-08 16:16:16 -0300

    Advanced Concepts of Mockito - Mocking Constructors and Static Methods with JUnit 5 and Mockito

--- S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/mockito/constructor/CheckoutService.java
+++ S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/mockito/constructor/CheckoutService.java
@@ -3,9 +3,11 @@ package br.com.erudio.mockito.constructor;
 import java.math.BigDecimal;
 
 public class CheckoutService {
-public BigDecimal purchaseProduct(String productName, String customerId) {
-// any arbitrary implementation
-PaymentProcessor paymentProcessor = new PaymentProcessor();
-return paymentProcessor.chargeCustomer(customerId, BigDecimal.TEN);
-}
+    
+    public BigDecimal purchaseProduct(String productName, String customerId) {
+        
+        // Any arbitrary implementation
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
+        return paymentProcessor.chargeCustomer(customerId, BigDecimal.TEN);
+    }
 }
--- S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/mockito/constructor/PaymentProcessor.java
+++ S07_AdvancedConceptsofMockito/src/main/java/br/com/erudio/mockito/constructor/PaymentProcessor.java
@@ -4,27 +4,28 @@ import java.math.BigDecimal;
 
 public class PaymentProcessor {
 
-private boolean allowForeignCurrencies;
-private String fallbackOption;
-private BigDecimal taxRate;
+    private boolean allowForeignCurrencies;
+    private String fallbackOption;
+    private BigDecimal taxRate;
 
-public PaymentProcessor() {
-this(false, "DEBIT", new BigDecimal("19.00"));
-}
+    public PaymentProcessor() {
+        this(false, "DEBIT", new BigDecimal("19.00"));
+    }
 
-public PaymentProcessor(String fallbackOption, BigDecimal taxRate) {
-this(false, fallbackOption, taxRate);
-}
+    public PaymentProcessor(String fallbackOption, BigDecimal taxRate) {
+        this(false, fallbackOption, taxRate);
+    }
 
-public PaymentProcessor(boolean allowForeignCurrencies, String fallbackOption, BigDecimal taxRate) {
-this.allowForeignCurrencies = allowForeignCurrencies;
-this.fallbackOption = fallbackOption;
-this.taxRate = taxRate;
-}
+    public PaymentProcessor(boolean allowForeignCurrencies, String fallbackOption, BigDecimal taxRate) {
+        this.allowForeignCurrencies = allowForeignCurrencies;
+        this.fallbackOption = fallbackOption;
+        this.taxRate = taxRate;
+    }
 
-public BigDecimal chargeCustomer(String customerId, BigDecimal netPrice) {
-// any arbitrary implementation
-System.out.println("About to charge customer: " + customerId);
-return BigDecimal.ZERO;
-}
+    public BigDecimal chargeCustomer(String customerId, BigDecimal netPrice) {
+        
+        // any arbitrary implementation
+        System.out.println("About to charge customer: " + customerId);
+        return BigDecimal.ZERO;
+    }
 }
