7a26e80b5a653d1c8d5c0ae5694da034621c64e9
leandrocgsi <leandrocgsi@gmail.com>
2023-04-17 14:15:16 -0300

    Integration Tests with JUnit 5, Mockito, RESTAssured and TestContainers

---
+++ S13_IntegrationTests/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,30 @@
+package br.com.erudio.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.erudio.config.TestConfigs;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest {
+
+    @Test
+    @DisplayName("JUnit test for Should Display Swagger UI Page")
+    void testShouldDisplaySwaggerUiPage() {
+        var content = given()
+            .basePath("/swagger-ui/index.html")
+            .port(TestConfigs.SERVER_PORT)
+            .when()
+                .get()
+            .then()
+                .statusCode(200)
+            .extract()
+                .body()
+                    .asString();
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
